03162abfe499a780683767f6033473f584b224e2
Benjamin Katz <bk@benjaminkatzcreative.com>
2012-06-23 11:16:08 -0700

    Add boilerplate

---
+++ wp-content/themes/bkc-theme/page.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+
+get_header(); ?>
+
+<div id="main" class="clearfix">
+	
+	
+	<?php if (is_page()) { ?>
+	<div>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div class="post" id="post-<?php the_ID(); ?>">
+			<div class="entry">
+				<?php the_content(); ?>
+				<?php endwhile; endif; ?>
+				<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+			</div>
+		</div>
+	</div>
+
+<?php } elseif (is_page('about')) { ?>
+	
+	<div>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div class="post" id="post-<?php the_ID(); ?>">
+			<div class="entry">
+				<?php the_content(); ?>
+				<?php endwhile; endif; ?>
+				<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+			</div>
+		</div>
+	</div>
+	
+	<?php get_sidebar(); ?>
+
+<?php } ?>
+
+</div>
+
+<?php get_footer(); ?>
